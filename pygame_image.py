e4a4b96f77e5e9f1148041ca6d5d05cfa1c062af
Kinjo Okuno <c0a23033dc@edu.teu.ac.jp>
2024-11-19 16:51:28 +0900

    課題１：自動的に左に流れるようにした

--- pygame_image.py
+++ pygame_image.py
@@ -10,7 +10,7 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)  # 背景画像を左右反転
-     # こうかとん画像を読み込み、左右反転
+    # こうかとん画像を読み込み、左右反転
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_rct = kk_img.get_rect()  # こうかとんRectを取得する
@@ -22,16 +22,16 @@ def main():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
-        # print(key_lst[pg.K_UP], key_lst[pg.K_DOWN], 
-        #       key_lst[pg.K_LEFT], key_lst[pg.K_RIGHT])
+        move_x, move_y = -1, 0  # デフォルトで左に移動
+
         if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
-            kk_rct.move_ip(0, -1)
+            move_y = -1
         if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
-            kk_rct.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
-            kk_rct.move_ip(-1, 0)
+            move_y = 1
         if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
-            kk_rct.move_ip(+1, 0)
+            move_x = 1
+
+        kk_rct.move_ip(move_x, move_y)
 
         x = -(tmr % 3200)  # 0から3199まで繰り返す値
 
