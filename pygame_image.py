58c7acfc7850ef4c32fdfcff8b2328e0c6b0b896
Kinjo Okuno <c0a23033dc@edu.teu.ac.jp>
2024-11-19 15:41:22 +0900

    練習5：FPSの変更

--- pygame_image.py
+++ pygame_image.py
@@ -24,7 +24,7 @@ def main():
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)#FPSを指定
 
 
 if __name__ == "__main__":
