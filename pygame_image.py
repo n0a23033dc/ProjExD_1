838ac5c505c399775ffab34cd5f90c6c577a75ae
Kinjo Okuno <c0a23033dc@edu.teu.ac.jp>
2024-11-19 16:28:35 +0900

    練習7：ワープの原因を取り除いた

--- pygame_image.py
+++ pygame_image.py
@@ -4,31 +4,30 @@ import pygame as pg
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    tmr = 0
-
-    # こうかとん画像を読み込み、左右反転
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 背景画像を左右反転
+     # こうかとん画像を読み込み、左右反転
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    tmr = 0
 
-    bg_x = 0  # 背景画像の初期横座標
+   
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        bg_x -= 1  # 背景画像を左に1ピクセルずつ移動
-        if bg_x <= -800:  # 背景画像が完全に左に移動したらリセット
-            bg_x = 0
+        x = -(tmr % 3200)  # 0から3199まで繰り返す値
 
-        # 背景画像を2つ並べて描画
-        screen.blit(bg_img, [bg_x, 0])
-        screen.blit(bg_img, [bg_x + 800, 0])
+        # 背景画像を4つ並べて描画
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img2, [x + 1600, 0])
+        screen.blit(bg_img, [x + 3200, 0])
+        screen.blit(bg_img2, [x + 4800, 0])
         
         # こうかとん画像を描画
         screen.blit(kk_img, [300, 200])
