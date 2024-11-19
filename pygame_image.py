7e486cb306c530425ef1544f2c911aacc56e11ea
Kinjo Okuno <c0a23033dc@edu.teu.ac.jp>
2024-11-19 16:36:18 +0900

    練習8：こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,13 +13,25 @@ def main():
      # こうかとん画像を読み込み、左右反転
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()  # こうかとんRectを取得する
+    kk_rct.center = 300, 200
     tmr = 0
-
-   
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        key_lst = pg.key.get_pressed()
+        # print(key_lst[pg.K_UP], key_lst[pg.K_DOWN], 
+        #       key_lst[pg.K_LEFT], key_lst[pg.K_RIGHT])
+        if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
+            kk_rct.move_ip(+1, 0)
 
         x = -(tmr % 3200)  # 0から3199まで繰り返す値
 
@@ -30,11 +42,11 @@ def main():
         screen.blit(bg_img2, [x + 4800, 0])
         
         # こうかとん画像を描画
-        screen.blit(kk_img, [300, 200])
+        screen.blit(kk_img, kk_rct)
         
         pg.display.update()
         tmr += 1        
-        clock.tick(60)  # FPSを指定
+        clock.tick(200)  # FPSを指定
 
 if __name__ == "__main__":
     pg.init()
